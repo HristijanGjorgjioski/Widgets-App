8a3325ce6b239cc132d7c62500350250857c0909
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-16 18:10:14 +0100

    successfyly fetch data from WikipediaAPI!

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,24 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('History');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    console.log('apala asasasassss');
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+    search();
   }, [term]);
 
   return (
