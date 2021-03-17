251c995599d456802618f27ca6623aa85dcdc89a
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-17 16:33:00 +0100

    using useEffect and useRef hooks in Dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener('click', (event) => {
+      console.log(event.target);
+      setOpen(false);
+    });
+  }, []);
 
   const renderedOptions = options.map((option, index) => {
     if (option.value === selected.value) {
@@ -20,7 +28,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select A Color</label>
         <div
